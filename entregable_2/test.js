bcc341ddb5d3d4a48999ce68e21bc91dfd1c080e
Juan Ignacio Padin <juanipadin@hotmail.com>
2021-09-22 12:52:54 -0300

    Add files via upload

---
+++ entregable_2/test.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+
+const Contenedor = require('./contenedor')
+
+const miContenedor = new Contenedor('productos.json');
+
+const misProductos = {
+    'nombre' : 'Pera',
+    'precio' : 11
+}
+
+const main = async () => {
+    const id = await miContenedor.save(misProductos)
+
+    await miContenedor.getById(id)
+
+    await miContenedor.getAll();
+    
+    await miContenedor.deleteById(id)
+    
+    await miContenedor.deleteAll()
+}
+
+main()
