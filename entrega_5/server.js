f1dd449d0993457ef589461e0f3e923a8f4f60fc
Juan Ignacio Padin <juanipadin@hotmail.com>
2021-10-11 02:35:38 -0300

    borrador

--- entrega_5/server.js
+++ entrega_5/server.js
@@ -4,13 +4,38 @@ const app = express()
 
 app.set('view engine', 'ejs');
 
-app.get("/datos", (req, res) => {
+const Contenedor = require('../entrega_3/contenedor');
+const productosContenedor = new Contenedor('/data/productos.json')
+
+/* app.get("/form", (req, res) => {
     res.render("pages/datos", {
-        min: req.query.min,
-        nivel: req.query.nivel,
-        max: req.query.max,
-        titulo: req.query.titulo,
-    });});
+        min : req.query.min,
+        nivel : req.query.nivel,
+        max : req.query.max,
+        titulo : req.query.titulo,
+    });}); */
+
+    app.get('/form', (req,res) =>{
+        res.render('pages/form',{
+            nombre : req.query.nombre,
+            precio : req.query.precio
+        })
+    })
+    
+app.post('/form', async (req, res) =>{
+    const newProducto = req.query; 
+    const idProductoNuevo = await productosContenedor.save(newProducto);
+    res.send({
+        message : 'success',
+        data: {
+            ...newProducto,
+            id: idProductoNuevo
+    }
+})
+})
+
+
+
 
 const PORT = 8080;
 app.listen(PORT, () => console.log(`Servidor iniciado en el puerto ${PORT}`));
