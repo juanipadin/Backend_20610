9c7a901e71d794d1bfb445d1de9e6b5534a913fc
Juan Ignacio Padin <juanipadin@hotmail.com>
2021-10-24 12:59:44 -0300

    se agrega chat y se corrigen errores menores

--- entrega_6/public/js/main.js
+++ entrega_6/public/js/main.js
@@ -1,8 +1,10 @@
 const socket = io.connect();
 
-const form = document.getElementById('form');
+/* CARGA DE LISTADO DE PRODUCTOS */
 
-form.addEventListener('submit', (event) => {
+const chatForm = document.getElementById('form');
+
+chatForm.addEventListener('submit', (event) => {
   event.preventDefault();
   const nombre = document.getElementById('nombre').value;
   const precio = document.getElementById('precio').value;
@@ -14,23 +16,47 @@ form.addEventListener('submit', (event) => {
 socket.on('productos', (productos) => {
     
   console.log(productos);
-  
-  
+    
   const productList = productos.map((product) => `
   <tr>
-          <td>
-            ${product.nombre}
-          </td>
-          <td>
-            ${product.precio}
-          </td>
-          <td>
-            <img src="${product.foto}" alt="" width="50" height="50">
-          </td>
-        </tr>
+    <td>
+      ${product.nombre}
+    </td>
+    <td>
+      ${product.precio}
+    </td>
+    <td>
+      <img src="${product.foto}" alt="" width="50" height="50">
+    </td>
+  </tr>
   `).join(' ');
   console.log(productList)
   const list = document.getElementById('real-time-products');
 
   list.innerHTML = productList;
+})
+
+/* CARGA CHAT */
+
+const form = document.getElementById('chat_form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const email = document.getElementById('username').value;
+  const texto = document.getElementById('texto').value;
+  
+  socket.emit('new-message', {email, texto});
+});
+
+socket.on('messages', (messages) => {
+  
+  const mensajeList = messages.map((message) =>`
+        <div>
+            <strong style="color:blue">${message.email}</strong> <em style="color:brown">[${message.fechaHora}]</em>:
+            <em style="color:green">${message.texto}</em> 
+        </div>
+        `).join(' ');
+  const renderMensajes = document.getElementById('lista_mensaje_final');
+
+  renderMensajes.innerHTML = mensajeList;
 })
--- entrega_6/server.js
+++ entrega_6/server.js
@@ -3,34 +3,57 @@ const { Server : SocketServer } = require('socket.io');
 const { Server : HttpServer} = require('http');
 const { connected } = require( 'process' );
 
-const Contenedor = require('./contenedor');
+/* CARGA DE PRODUCTOS */
+const Contenedor = require('./models/contenedor');
 const productosContenedor = new Contenedor('/data/productos.json')
+
+/* CARGA DE CHAT */
+const Mensajes = require( './models/messages.js' );
+const mensajes = new Mensajes('/data/messages.json')
 
 const app = express();
 const httpServer = new HttpServer(app);
 const io = new SocketServer(httpServer);
 
+/* HABILITA EL USO DEL JSON */
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
+/* CREA RELACIÓN A CARPETA 'PUBLIC' */
 app.use( express.static('public') );
 
+/* HABILITA EL USO DE EJS */
 app.set('view engine', 'ejs');
 
+/* CREA EL WEBSOCKET */
 io.on('connection', async (socket) => {
     console.log(`Nuevo cliente conectado ${socket.id}`)
-    
+
+    /* CARGA DE PRODUCTOS INGRESADOS */
     socket.on('new-product', async(producto) =>{
         await productosContenedor.save(producto);
         const productos = await productosContenedor.getAll()
         io.sockets.emit('productos', productos) 
-    } )
+    })
+
+    /* CARGA EL CHAT */
+    const messages = await mensajes.getMessagess();
+    socket.emit('messages', messages);
+
+    socket.on('new-message', async data => {
+        data.fechaHora = new Date().toLocaleString();
+        await mensajes.saveMessages(data);
+        const messages = await mensajes.getMessages();
+        io.sockets.emit('messages', messages);
+    }) 
 })
 
+/* CREA EL /FORM */
 app.get('/form', async (req, res) =>{
     res.render ('../views/pages/form')
 })
 
+/* CREA EL /POST PRODUCTOS */
 app.post('/productos', async (req, res) =>{
     const newProducto = req.body; 
     console.log(newProducto)
@@ -38,6 +61,7 @@ app.post('/productos', async (req, res) =>{
     res.redirect('/list-productos');
 }) 
 
+/* CREA LISTA DE PRODUCTOS */
 app.get('/list-productos', async (req, res) =>{
     const productos = await productosContenedor.getAll()
     res.render('pages/vista_producto',{
@@ -45,8 +69,7 @@ app.get('/list-productos', async (req, res) =>{
     })
 })
 
-
-
+/* REALIZA LA CONECCIÓN Y VERIFICA ERRORES */
 const PORT = 8080;
 const connectedServer = httpServer.listen(PORT, () => {
     console.log(`Servidor en Http con Websocket escuchando en el puerto ${connectedServer.address().port}`)
