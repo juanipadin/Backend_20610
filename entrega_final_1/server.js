aea5a0594ae9e784b23d325228c1f57648558156
Juan Ignacio Padin <juanipadin@hotmail.com>
2021-10-26 15:24:38 -0300

    Se agrega /productos

--- entrega_final_1/server.js
+++ entrega_final_1/server.js
@@ -1,81 +1,20 @@
 const express = require('express');
-const { Server : SocketServer } = require('socket.io');
-const { Server : HttpServer} = require('http');
-const { connected } = require( 'process' );
+const { appendFile } = require( 'fs' );
+const carritoRouter = require( '../entrega_final_1/routers/carrito' );
+const productosRouter = require( '../entrega_final_1/routers/productos' );
 
-/* CARGA DE PRODUCTOS */
-const Contenedor = require('./models/contenedor');
-const productosContenedor = new Contenedor('/data/productos.json')
+const server = express();
 
-/* CARGA DE CHAT */
-const Mensajes = require( './models/messages.js' );
-const mensajes = new Mensajes('/data/messages.json')
-
-const app = express();
-const httpServer = new HttpServer(app);
-const io = new SocketServer(httpServer);
-
-/* HABILITA EL USO DEL JSON */
-app.use(express.json());
-app.use(express.urlencoded({extended: true}))
-
-/* CREA RELACIÓN A CARPETA 'PUBLIC' */
-app.use( express.static('public') );
-
-/* HABILITA EL USO DE EJS */
-app.set('view engine', 'ejs');
-
-/* CREA EL WEBSOCKET */
-io.on('connection', async (socket) => {
-    console.log(`Nuevo cliente conectado ${socket.id}`)
-
-    const productos = await productosContenedor.getAll();
-    socket.emit('productos', productos);
-
-    /* CARGA DE PRODUCTOS INGRESADOS */
-    socket.on('new-product', async(producto) =>{
-        await productosContenedor.save(producto);
-        const productos = await productosContenedor.getAll()
-        io.sockets.emit('productos', productos) 
-    })
-
-    /* CARGA EL CHAT */
-    const messages = await mensajes.getMessagess();
-    socket.emit('messages', messages);
-
-    socket.on('new-message', async data => {
-        data.fechaHora = new Date().toLocaleString();
-        await mensajes.saveMessages(data);
-        const messages = await mensajes.getMessages();
-        io.sockets.emit('messages', messages);
-    }) 
-})
-
-/* CREA EL /FORM */
-app.get('/form', async (req, res) =>{
-    res.render ('../views/pages/form')
-})
-
-/* CREA EL /POST PRODUCTOS */
-app.post('/productos', async (req, res) =>{
-    const newProducto = req.body; 
-    console.log(newProducto)
-    const idProductoNuevo = await productosContenedor.save(newProducto);
-    res.redirect('/list-productos');
-}) 
-
-/* CREA LISTA DE PRODUCTOS */
-app.get('/list-productos', async (req, res) =>{
-    const productos = await productosContenedor.getAll()
-    res.render('pages/vista_producto',{
-        productos : productos,
-    })
-})
-
-/* REALIZA LA CONECCIÓN Y VERIFICA ERRORES */
 const PORT = 8080;
-const connectedServer = httpServer.listen(PORT, () => {
-    console.log(`Servidor en Http con Websocket escuchando en el puerto ${connectedServer.address().port}`)
-})
 
-connectedServer.on('error', error => console.log(`Eror en el servidor ${PORT}`))
+server.use(express.json());
+server.use(express.urlencoded({extended: true}))
+
+server.use('/api',express.static('public'))
+
+server.use('/api/productos', productosRouter)
+server.use('/api/carrito', carritoRouter)
+
+server.listen(PORT, () => console.log(`Servidor corriendo en ${PORT}`));
+
+server.on('error', error => console.log('Error en servidor: ', error))
