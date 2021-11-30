c6081e6b3f880b8b4f7bd29fac8d0f635d156b2c
juanipadin <juanipadin@hotmail.com>
2021-11-30 14:48:00 -0300

    Se realizan modificaciones menores

--- entrega_11/models/messages.js
+++ entrega_11/models/messages.js
@@ -1,42 +1,3 @@
-const messages = [
-  { 
-    id: 0,
-    author: {
-      id: 'mail del usuario', 
-      nombre: 'nombre del usuario', 
-      apellido: 'apellido del usuario', 
-      edad: 'edad del usuario', 
-      alias: 'alias del usuario',
-      avatar: 'url avatar (foto, logo) del usuario'
-    },
-    text: "¡Hola! ¿Que tal?"
-  },
-  { 
-    id: 1,
-    author: {
-      id: 'mail del usuario', 
-      nombre: 'nombre del usuario', 
-      apellido: 'apellido del usuario', 
-      edad: 'edad del usuario', 
-      alias: 'alias del usuario',
-      avatar: 'url avatar (foto, logo) del usuario'
-    },
-    text: "¡Muy bien! ¿Y vos?"
-  },
-  { 
-    id: 2,
-    author: {
-      id: 'mail del usuario', 
-      nombre: 'nombre del usuario', 
-      apellido: 'apellido del usuario', 
-      edad: 'edad del usuario', 
-      alias: 'alias del usuario',
-      avatar: 'url avatar (foto, logo) del usuario'
-    },
-    text: "¡Genial!"
-  }
-];
-
 const normalizeMessages = require( '../utils/normalizar' );
 
 const Mensajes = require( '../models/messages_archivo' );
@@ -47,8 +8,10 @@ const saveMessages = async (newMessage) => {
     return mensajeNuevo
 }
 
+
 const getMessages = async () => {
-    return normalizeMessages({ id: 'messages', messages });
+  const mensajes = await messageContenedor.getMessagess()
+  return normalizeMessages({ id: 'messages', mensajes});
     
 }; 
 
