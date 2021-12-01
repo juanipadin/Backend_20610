b45d049086437f8e6095f1c131b08c6c5207a98d
Juan Ignacio Padin <juanipadin@hotmail.com>
2021-12-01 10:21:33 -0300

    Se modifica para que resulte operativo

--- entrega_final_2/src/daos/index.js
+++ entrega_final_2/src/daos/index.js
@@ -5,23 +5,51 @@ const ProductosDaoMongo = require( './productos/ProductosDaoMongo' );
 const ProductosDaoMemoria = require( './productos/ProductosDaoMemoria' );
 const ProductosDaoFirestore = require( './productos/ProductosDaoFirestore' );
 
+const CarritoDaoArchivos = require('./carrito/CarritoDaoArchivos');
+const CarritoDaoMongo = require( './carrito/CarritoDaoMongo' );
+const CarritoDaoMemoria = require( './carrito/CarritoDaoMemoria' );
+const CarritoDaoFirestore = require( './carrito/CarritoDaoFirestore' );
+
 const daos = {}
+
+/* ENV PRODUCTOS */
 daos['ProductDao'] = ProductosDaoMongo
 
-if (process.env.storage === 'mongodb') {
+if (process.env.STORAGE === 'mongodb') {
     daos['ProductDao'] = ProductosDaoMongo;
 }
 
-if (process.env.storage === 'memory') {
+if (process.env.STORAGE === 'memory') {
     daos['ProductDao'] = ProductosDaoMemoria;
 }
 
-if (process.env.storage === 'firestore') {
+if (process.env.STORAGE === 'firestore') {
     daos['ProductDao'] = ProductosDaoFirestore; 
 }
 
-if (process.env.storage === 'file') {
+if (process.env.STORAGE === 'file') {
     daos['ProductDao'] = ProductosDaoArchivos;
+}
+
+
+
+/* ENV CARRITO */
+daos['CartDao'] = CarritoDaoMongo
+
+if (process.env.STORAGE === 'mongodb') {
+    daos['CatDao'] = CarritoDaoMongo;
+}
+
+if (process.env.STORAGE === 'memory') {
+    daos['CatDao'] = CarritoDaoMemoria;
+}
+
+if (process.env.STORAGE === 'firestore') {
+    daos['CatDao'] = CarritoDaoFirestore; 
+}
+
+if (process.env.STORAGE === 'file') {
+    daos['CatDao'] = CarritoDaoArchivos;
 }
 
 module.exports = daos;
--- entrega_final_2/src/routers/productos.js
+++ entrega_final_2/src/routers/productos.js
@@ -7,43 +7,25 @@ const productDao = new ProductDao();
 
 const isAdmin = require( '../../middlewares/isAdmin' );
 
-const ProductosDaoMongo = require( '../daos/productos/ProductosDaoMongo' );
-const ProductosDaoArchivos = require( '../daos/productos/ProductosDaoArchivos' );
-const ProductosDaoMemoria = require( '../daos/productos/ProductosDaoMemoria' );
-const ProductosDaoFirestore = require( '../daos/productos/ProductosDaoFirestore' );
-
-
-const productoDaoArchivo = new ProductosDaoArchivos();
-const productoDaoMongo = new ProductosDaoMongo();
-const productoDaoMemoria = new ProductosDaoMemoria();
-const productoDaoFirestore = new ProductosDaoFirestore();
 
 productosRouter.get('/', async (req, res) =>{
     console.log(productDao)
     const data = await productDao.getAll();
-    const data = await productoDaoArchivo.getAll();
-    //const data = await productoDaoMemoria.getAll();
-    //const data = await productoDaoFirestore.getAll();
-    //const data = await productoDaoMongo.getAll()
     
     res.send({data})
 })
 
 productosRouter.get('/:id', async (req, res) =>{
     const idProducto = req.params.id
-    const data = await productoDaoArchivo.getById(idProducto)
-    //const data = await productoDaoMemoria.getById(idProducto)
-    //const data = await productoDaoMongo.getById(idProducto)
-    //const data = await productoDaoFirestore.getById(idProducto)
+    const data = await productDao.getById(idProducto)
+
     res.send({data})
 })
 
 productosRouter.post('/', isAdmin, async (req, res) =>{
     const newProducto = req.body; 
-    const idProductosSaved = await productoDaoArchivo.save(newProducto)
-    //const idProductosSaved = await productoDaoMemoria.save(newProducto)
-    //const idProductosSaved = await productoDaoMongo.save(newProducto)
-    //const idProductosSaved = await productoDaoFirestore.save(newProducto)
+    const idProductosSaved = await productDao.save(newProducto)
+
     res.send({
         message : 'Se registró con éxito el producto',
         data: { idProductosSaved }})
@@ -52,19 +34,14 @@ productosRouter.post('/', isAdmin, async (req, res) =>{
 productosRouter.put('/:id', isAdmin, async (req, res) =>{
     const datosNuevos = req.body;
     const idProducto = req.params.id;
-    const data = await productoDaoArchivo.update(idProducto,datosNuevos)
-    //const idProductosSaved = await productoDaoMemoria.update(idProducto,datosNuevos)
-    //const data = await productoDaoMongo.update(idProducto,datosNuevos);
-    //const data = await productoDaoFirestore.update(idProducto,datosNuevos);
+    const data = await productDao.update(idProducto,datosNuevos)
+
     res.send({data})
 })
 
 productosRouter.delete('/:id', isAdmin, async (req, res) =>{
     idProducto = req.params.id
-    const data = await productoDaoArchivo.deleteById(idProducto)
-    //const data = await productoDaoMemoria.deleteById(idProducto)
-    //const data = await productoDaoMongo.deleteById(idProducto)
-    res//const data = await productoDaoFirestore.deleteById(idProducto).send({data})
+    const data = await productDao.deleteById(idProducto)
     
     res.send({data})
 })
