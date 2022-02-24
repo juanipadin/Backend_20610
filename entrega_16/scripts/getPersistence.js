d8813db97f876725c9c56ea4e3c18e097e5e43ed
juanipadin <juanipadin@hotmail.com>
2022-02-24 20:26:56 -0300

    Desafío: "Mejorar la arquitectura de nuestra API"

--- entrega_16/scripts/getPersistence.js
+++ entrega_16/scripts/getPersistence.js
@@ -2,7 +2,7 @@ const { PERS } = require("../src/config")
 
 const getPersistence = () => {
     console.log(process.argv[2])
-    return PERS.fileSystem
+    return PERS
   }
   
   module.exports = getPersistence
